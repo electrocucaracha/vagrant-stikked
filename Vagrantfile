f0a4fd17eb07aaff1f5d44123d3b97d631a5a207
Victor Morales <victor.morales@intel.com>
2017-07-04 11:50:33 -0500

    Fix installation process
    It was assigned a private IP for its deployment.  Reduced the amount
    of instructions and included some missing configuration values

--- Vagrantfile
+++ Vagrantfile
@@ -8,6 +8,6 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.hostname = "stikked-server"
-  config.vm.network :forwarded_port, guest: 80, host: 8080
+  config.vm.network :private_network, ip: "10.0.0.2"
   config.vm.provision "shell", path: "postinstall.sh"
 end
--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Workaround
-
-sed -i "s/nameserver 10.0.2.3/nameserver 8.8.8.8/g" /etc/resolv.conf
-
 # Configuration
 
 root_db_password=secure
@@ -11,7 +7,7 @@ db_username=stikked
 db_password=stikked_admin
 db_name=stikked
 
-apt-get update && apt-get dist-upgrade
+apt-get update -y
 
 # Database
 
@@ -19,28 +15,30 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password password $ro
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $root_db_password"
 apt-get install -y mysql-server
 
-sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/my.cnf
-
 service mysql restart
 
 mysql -p$root_db_password -e "CREATE DATABASE $db_name;"
-mysql -p$root_db_password -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$stikked'@'localhost' IDENTIFIED BY '$db_password';"
-mysql -p$root_db_password -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$stikked'@'%' IDENTIFIED BY '$db_password';"
+mysql -p$root_db_password -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_username'@'localhost' IDENTIFIED BY '$db_password';"
 
-#echo -e "$root_db_password\nn\nY\nY\n\Y\n" | mysql_secure_installation
+echo -e "$root_db_password\nn\nY\nY\n\Y\n" | mysql_secure_installation
 
 # Web Server
 
 apt-get install -y git  apache2 php5 libapache2-mod-php5 php5-mysql php5-ldap
 
-git config --global url.https://.insteadof git://
 git clone https://github.com/claudehohl/Stikked.git /var/www/Stikked
 
-pushd /var/www/Stikked
-cp htdocs/application/config/stikked.php.dist htdocs/application/config/stikked.php
-sed -i "s/\$config\['db_username'\] = 'stikked';/\$config\['db_username'\] = '$db_username';/g" htdocs/application/config/stikked.php
-sed -i "s/\$config\['db_password'\] = 'stikked';/\$config\['db_password'\] = '$db_password';/g" htdocs/application/config/stikked.php
-sed -i "s/\$config\['enable_captcha'\] = true;/\$config\['enable_captcha'\] = false;/g" htdocs/application/config/stikked.php
+pushd /var/www/Stikked/htdocs
+mv application/config/stikked.php.dist application/config/stikked.php
+sed -i "s/\$config\['base_url'\] = 'https:\/\/yourpastebin.com\/';/\$config\['base_url'\] = 'http:\/\/10.0.0.2\/';/g" application/config/stikked.php
+
+sed -i "s/\$config\['db_username'\] = 'stikked';/\$config\['db_username'\] = '$db_username';/g" application/config/stikked.php
+sed -i "s/\$config\['db_password'\] = 'stikked';/\$config\['db_password'\] = '$db_password';/g" application/config/stikked.php
+sed -i "s/\$config\['enable_captcha'\] = true;/\$config\['enable_captcha'\] = false;/g" application/config/stikked.php
+
+chmod a+w static/asset
+sed -i "s/\$config\['combine_assets'\] = false;/\$config\['combine_assets'\] = true;/g" application/config/stikked.php
+popd
 
 rm /etc/apache2/sites-available/*
 rm /etc/apache2/sites-enabled/*
@@ -59,8 +57,6 @@ cat <<EOL > /etc/apache2/sites-enabled/stikked.conf
     </Directory>
 </VirtualHost>
 EOL
-
-popd
 
 a2enmod php5 rewrite
 a2ensite stikked
